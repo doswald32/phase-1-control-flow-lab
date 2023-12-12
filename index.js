da92e5e11b5a6ab5ef5a575e99597b60d82b0279
doswald32 <dan.oswald32@gmail.com>
2023-12-11 21:46:21 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let replyOne = 'This one is on me!';
+  let replyTwo = 'That will be twenty bucks.';
+  let replyThree = 'I will gladly take your thirty bucks.';
+  let replyFour = 'No can do.';
+  if(feet <=400) {
+    return replyOne;
+  } else if(feet >=400 && feet <=2000) {
+    return replyTwo;
+  } else if(feet > 2000 && feet <= 2500) {
+    return replyThree;
+  } else if(feet >2500) {
+    return replyFour;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let tipResponse;
+  switch (tip) {
+    case tip = 'generous':
+      return tipResponse = 'Thank you so much.';
+      break;
+    case tip = 'not as generous':
+      return tipResponse = 'Thank you.';
+      break;
+    default:
+      return tipResponse = 'Bye.';
+  }
 }
